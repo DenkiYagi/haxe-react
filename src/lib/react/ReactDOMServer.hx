a2379f6aa50676acf2a8bb09c709a49d7bda3ad2
k <k@klabz.org>
2023-10-12 16:04:37 +0200

    Update and document ReactDOM API

--- src/lib/react/ReactDOMServer.hx
+++ src/lib/react/ReactDOMServer.hx
@@ -1,43 +1,113 @@
 package react;
 
+import js.html.AbortSignal;
+import js.lib.Promise;
 import react.ReactComponent.ReactFragment;
 
 #if nodejs
 import js.node.stream.Readable;
-
-@:native('ReactMarkupReadableStream')
-@:jsRequire('react-dom/server/ReactDOMNodeStreamRenderer', 'ReactMarkupReadableStream')
-class ReactMarkupReadableStream extends Readable<ReactMarkupReadableStream> {}
+import js.node.stream.Writable;
 #end
 
 /**
-	https://facebook.github.io/react/docs/react-dom-server.html
+	The `ReactDOMServer` APIs let you render React components to HTML on the
+	server. These APIs are only used on the server at the top level of your app to
+	generate the initial HTML. Most of your components don’t need to import or use them.
+
+	https://react.dev/reference/react-dom/server
 **/
 #if (!react_global)
 @:jsRequire('react-dom/server')
 #end
 @:native('ReactDOMServer')
-extern class ReactDOMServer
-{
+extern class ReactDOMServer {
 	/**
-		https://reactjs.org/docs/react-dom-server.html#rendertostring
+		Note: `renderToString` does not support streaming or waiting for data.
+		See the alternatives.
+
+		https://react.dev/reference/react-dom/server/renderToString
 	**/
 	public static function renderToString(node:ReactFragment):String;
 
 	/**
-		https://reactjs.org/docs/react-dom-server.html#rendertostaticmarkup
+		`renderToStaticMarkup` renders a non-interactive React tree to an HTML
+		string.
+
+		Notes:
+		- `renderToStaticMarkup` output cannot be hydrated.
+		- `renderToStaticMarkup` has limited `Suspense support`. If a component
+		  suspends, `renderToStaticMarkup` immediately sends its fallback as HTML.
+
+		https://react.dev/reference/react-dom/server/renderToStaticMarkup
 	**/
 	public static function renderToStaticMarkup(node:ReactFragment):String;
 
 	#if nodejs
 	/**
-		https://reactjs.org/docs/react-dom-server.html#rendertonodestream
+		`renderToPipeableStream` renders a React tree to a pipeable Node.js Stream.
+
+		Note: This API is specific to Node.js.
+		Environments with Web Streams should use `renderToReadableStream` instead.
+
+		https://react.dev/reference/react-dom/server/renderToPipeableStream
 	**/
-	public static function renderToNodeStream(node:ReactFragment):ReactMarkupReadableStream;
+	public static function renderToPipeableStream(node:ReactFragment, options:{
+		?bootstrapScriptContent:String,
+		?bootstrapScripts:Array<String>,
+		?bootstrapModules:Array<String>,
+		?identifierPrefix:String,
+		?namespaceURI:String,
+		?nonce:String,
+		?onAllReady:Void->Void,
+		?onError:Any->Void,
+		?onShellReady:Void->Void,
+		?onShellError:Any->Void,
+		?progressiveChunkSize:Int
+	}):{
+		pipe:IWritable->Void,
+		abort:Void->Void
+	}
 
 	/**
-		https://reactjs.org/docs/react-dom-server.html#rendertostaticnodestream
+		`renderToStaticNodeStream` renders a non-interactive React tree to a
+		Node.js Readable Stream.
+
+		Notes:
+		- `renderToStaticNodeStream` output cannot be hydrated.
+		- This method will wait for all `Suspense` boundaries to complete before
+		  returning any output.
+		- As of React 18, this method buffers all of its output, so it doesn't
+		  actually provide any streaming benefits.
+		- The returned stream is a byte stream encoded in utf-8
+
+		https://react.dev/reference/react-dom/server/renderToStaticNodeStream
 	**/
-	public static function renderToStaticNodeStream(node:ReactFragment):ReactMarkupReadableStream;
+	public static function renderToStaticNodeStream(node:ReactFragment):IReadable;
+
+	/**
+		https://react.dev/reference/react-dom/server/renderToNodeStream
+	**/
+	@:deprecated("Use renderToPipeableStream instead")
+	public static function renderToNodeStream(node:ReactFragment):IReadable;
+	#else
+	/**
+		`renderToReadableStream` renders a React tree to a Readable Web Stream.
+
+		Note: This API depends on Web Streams.
+		For Node.js, use `renderToPipeableStream` instead.
+
+		https://react.dev/reference/react-dom/server/renderToReadableStream
+	**/
+	public static function renderToReadableStream(node:ReactFragment, options:{
+		?bootstrapScriptContent:String,
+		?bootstrapScripts:Array<String>,
+		?bootstrapModules:Array<String>,
+		?identifierPrefix:String,
+		?namespaceURI:String,
+		?nonce:String,
+		?onError:Any->Void,
+		?progressiveChunkSize:Int,
+		?signal:AbortSignal
+	}):Promise<Dynamic /* TODO needs to extend non existing haxe std ReadableStream */>;
 	#end
 }
