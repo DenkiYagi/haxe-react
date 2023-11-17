53cc3affd8d2952891d67ef40a63f91cd9e29834
terurou <terurou@denkiyagi.jp>
2023-11-17 16:12:14 +0900

    Fix `ReactRef`

--- src/lib/react/ReactRef.hx
+++ src/lib/react/ReactRef.hx
@@ -4,10 +4,14 @@ import haxe.Constraints.Function;
 
 @:callable
 abstract ReactRef<T>(Function) {
-	public var current(get, never):T;
+	public var current(get, set):T;
 
 	public function get_current():T {
 		return untyped this.current;
+	}
+
+	public function set_current(value:T):T {
+		return untyped this.current = value;
 	}
 }
 
